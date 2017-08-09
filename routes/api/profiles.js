97f5f5f6ea2fcfb197f97bc1167355840c93411f
Dominic Chapman <dominicchapman@me.com>
2017-08-09 17:50:55 +0100

    parameter middleware and get route for profiles/:username

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -12,4 +12,20 @@ var auth = require('../auth');
 	3. Create routes for following and unfollowing profiles.
 */
 
+// prepopulate req.profile with user's data when :username paramater is contained within a route.
+router.param('username', function(req, res, next, username){
+	User.findOne({username: username}).then(function(user){
+		if(!user) { return res.sendStatus(404); }
+
+		req.profile = user;
+
+		return next();
+	}).catch(next);
+});
+
+// endpoint to fetch user's profile based on username using the parameter middleware.
+router.get('/:username', auth.optional, function(req, res, next){
+	return res.json({profile: req.profile.toProfileJSONFor()});
+});
+
 module.exports = router;
