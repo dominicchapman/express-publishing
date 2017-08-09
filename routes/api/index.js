c865afbdbee88ede4f7dc7cd608dcd790d8a7bd8
Dominic Chapman <dominicchapman@me.com>
2017-08-09 15:46:00 +0100

    error handling middleware

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,4 +2,19 @@ var router = require('express').Router();
 
 router.use('/', require('./users'));
 
+// middleware function to convert mongoose validation errors to consumable messages on front-end.
+router.use(function(err, req, res, next) { // middleware with four arguments treated as error handler
+	if(err.name === 'ValidationError') {
+		return res.status(422).json({
+			errors: Object.keys(err.errors).reduce(function(errors, key) {
+				errors[key] = err.errors[key].message;
+
+				return errors;
+			}, {})
+		});
+	}
+
+	return next(err);
+})
+
 module.exports = router;
