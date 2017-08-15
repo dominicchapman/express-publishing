223f3c9260e15ea0799f26bf4ee5f39bcf502378
Dominic Chapman <dominicchapman@me.com>
2017-08-15 16:50:48 +0100

    add endpoint for following another user

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -37,4 +37,17 @@ router.get('/:username', auth.optional, function(req, res, next){
 	}
 });
 
+// endpoint for following another user.
+router.post('/:username/follow', auth.required, function(req, res, next) {
+	var profileId = req.profile._id;
+
+	User.findById(req.payload.id).then(function(user) {
+		if(!user) { return res.sendStatus(401); }
+
+		return user.follow(profileId).then(function() {
+			return res.json({profile: req.profile.toProfileJSONFor(user)});
+		});
+	}).catch(next);
+});
+
 module.exports = router;
