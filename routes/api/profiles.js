8874abee612fdf7cca7a35300a77c8fa6fc58a8a
Dominic Chapman <dominicchapman@me.com>
2017-08-15 16:55:12 +0100

    add endpoint for unfollowing another user

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -50,4 +50,17 @@ router.post('/:username/follow', auth.required, function(req, res, next) {
 	}).catch(next);
 });
 
+// endpoint for unfollowing another user.
+router.delete('/:username/follow', auth.required, function(req, res, next) {
+	var profileId = req.profile._id;
+
+	User.findById(req.payload.id).then(function(user) {
+		if(!user) { return res.sendStatus(401); }
+
+		return user.unfollow(profileId).then(function() {
+			return res.json({profile: req.profile.toProfileJSONFor(user)});
+		});
+	}).catch(next);
+});
+
 module.exports = router;
