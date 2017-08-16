59d2dd93ef888205d177cc5a47731d7cbe4f98f5
Dominic Chapman <dominicchapman@me.com>
2017-08-16 22:11:08 +0100

    create and register tags router

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
 router.use('/articles', require('./articles'));
+router.use('/tags', require('./tags'));
 
 // middleware function to convert mongoose validation errors to consumable messages on front-end.
 router.use(function(err, req, res, next) { // middleware with four arguments treated as error handler
---
+++ routes/api/tags.js
@@ -0,0 +1,5 @@
+var router = require('express').Router();
+var mongoose = require('mongoose');
+var Article = mongoose.model('Article');
+
+module.exports = router;
