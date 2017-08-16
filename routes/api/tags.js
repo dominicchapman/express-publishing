3017787e8e2396e570fc0e4bb108280a9194bde9
Dominic Chapman <dominicchapman@me.com>
2017-08-16 22:17:00 +0100

    add route for retrieving tags used on articles

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -2,4 +2,12 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
 
+// route for retrieving tags used on articles.
+router.get('/', function(req, res, next) {
+	// using find and distinct Mongoose methods, we gather list of unique tags added to articles.
+	Article.find().distinct('tagList').then(function(tags) {
+		return res.json({tags: tags});
+	}).catch(next);
+});
+
 module.exports = router;
